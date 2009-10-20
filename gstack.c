0ee6e4a7351940923f05915569d6733f127552f4
Kazuki UCHIDA <gioext@gmail.com>
2009-10-20 19:25:07 +0900

    revert

--- gstack.c
+++ gstack.c
@@ -12,7 +12,6 @@ gstack_t *gstack_new()
         return NULL;
     }
     stack->length = 0;
-    stack->wait = 0;
     stack->head = NULL;
     if (pthread_mutex_init(&(stack->mutex), NULL) < 0) {
         fprintf(stderr, "mutex_init error\n");
@@ -62,7 +61,6 @@ void *gstack_pop(gstack_t *stack)
 
     pthread_mutex_lock(&(stack->mutex));
     while (stack->length == 0) {
-        stack->wait = 1;
         pthread_cond_wait(&(stack->cond), &(stack->mutex));
     }
 
@@ -70,7 +68,6 @@ void *gstack_pop(gstack_t *stack)
     datap = node->datap;
     stack->head = node->nextp;
     stack->length--;
-    stack->wait = 0;
     free(node);
     pthread_mutex_unlock(&(stack->mutex));
 
